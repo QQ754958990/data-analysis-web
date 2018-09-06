e1f18a5aad9461bf9bcae903505fe3fe35f378e7
shenlian <754958990@qq.com>
2018-09-06 14:41:34 +0800

    Adjust the font

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,29 @@ export default {
 
 <style lang="scss">
   @import url('../static/css/grid.css');
-  html,body,#app{
-    height: 100%;
-    background-color: #f2f2f2;
+
+  @media screen and (min-width:900px){
+   html{
+     font-size: 15px;
+   }
   }
 
+  @media screen and (min-width:600px) and (max-width:900px){
+    html{
+      font-size: 10px;
+    }
+  }
+
+  @media screen and (max-width:600px){
+    html{
+      font-size: 5px;
+    }
+  }
+
+  html,body,#app{
+    min-width: 600px;
+    height: 100%;
+    background-color: #f2f2f2;
+    overflow: auto;
+  }
 </style>
